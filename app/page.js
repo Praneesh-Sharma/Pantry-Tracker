918688ca3f539d408e8a863a7fc111a9fff22120
Praneesh-Sharma <pegasus30042004@gmail.com>
2024-08-08 22:56:59 +0530

    Added all files

--- app/page.js
+++ app/page.js
@@ -1,95 +1,161 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { firestore } from '@/firebase';
+import {Box, Typography, Stack, Button, Modal, TextField} from '@mui/material'
+import { Firestore, getDoc, query, collection, getDocs, setDoc, doc, deleteDoc  } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+
+// const items = ['tomato','potato','onion','garlic','ginger','carrot','lettuce','kale','cucumber']
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'white',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 3,
+};
 
 export default function Home() {
+  const [pantry, setPantry] = useState([])
+
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  const [itemName, setItemName] = useState('')
+
+  const updatePantry = async () => {
+    const snapshot = query(collection(firestore, 'pantry'))
+    const docs = await getDocs(snapshot)
+    const pantryList = []
+    docs.forEach((doc) => {
+      // console.log(doc.id)
+      pantryList.push({"name": doc.id, ...doc.data()})
+    })
+    console.log(pantryList)
+    setPantry(pantryList)
+  }
+
+  useEffect(() => {
+    updatePantry()
+  }, [])
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item)
+    const docSnap = await getDoc(docRef)
+    // console.log(docSnap.data())
+    if (docSnap.exists()) {
+      const {count} = docSnap.data()
+      await setDoc (docRef, {count: count + 1})
+    } else {
+      await setDoc(docRef, {count: 1})
+    }
+    await updatePantry()
+  }
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item)
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      const {count} = docSnap.data()
+      if (count === 1){
+        await deleteDoc(docRef)
+      } else {
+        await setDoc(docRef, {count: count - 1})
+      }
+    }
+    await updatePantry()
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <Box 
+    width="200vh" 
+    height="100vh"
+    display={'flex'}
+    justifyContent={'center'}
+    flexDirection={'column'}
+    alignItems={'center'}
+    gap={2}
+    >
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack width='100%' direction={'row'} spacing={2}>
+            <TextField 
+              id="outlined-basic" 
+              label="Item" 
+              variant='outlined' 
+              fullWidth
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}  
             />
-          </a>
-        </div>
-      </div>
+            <Button variant='outlined'
+              onClick={() => {
+                addItem(itemName)
+                setItemName('')
+                handleClose()
+              }}
+            >Add</Button>
+          </Stack>
+        </Box>
+      </Modal>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Button variant='contained' onClick={handleOpen}>Add</Button>
+      <Box border={'2px solid black'}>
+        <Box width='800px' height='100px' bgcolor={'#ADD8E6'} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+          <Typography variant='h2' color={'#333'} textAlign={'center'}>
+            Pantry Items
+          </Typography>
+          </Box> 
+        <Stack width="800px" height="300px" spacing={2} overflow={'auto'}>
+          {pantry.map((item) => (
+            <Stack key={item.name} direction={'row'} spacing={2} justifyContent={'center'} alignContent={'space-between'}>
+              <Box
+                key={item.name}
+                width="100%"
+                minHeight="150px"
+                display={'flex'}
+                justifyContent={'space-between'}
+                paddingX={4}
+                alignItems={'center'}
+                bgcolor={'#f0f0f0'}
+              >
+                <Typography
+                  variant={'h3'}
+                  color={'#333'}
+                  textAlign={'center'}
+                  // fontWeight={"bold"}
+                >
+                  {
+                    //Capitalize the first letter of the item
+                    item.name.charAt(0).toUpperCase() + item.name.slice(1)
+                  }
+                </Typography>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+                <Typography variant='h3' color={'#333'} textAlign={'center'}>
+                  Quantity: {item.count}
+                </Typography>
+              </Box>
+              <Button variant='contained' onClick={() => removeItem(item.name)}>Remove</Button>
+            </Stack>
+          ))}
+        </Stack>
+      </Box>
+    </Box>
   );
 }
---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBwhMRd99KhFgnWzYJ750ziouqjbo5uwI",
+  authDomain: "pantry-tracker-app-cd5b3.firebaseapp.com",
+  projectId: "pantry-tracker-app-cd5b3",
+  storageBucket: "pantry-tracker-app-cd5b3.appspot.com",
+  messagingSenderId: "573700541413",
+  appId: "1:573700541413:web:460fc56211a956630d7d0c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+export{app, firestore}
